18328ea13b689f778389b592735c2ebc688e5aea
yassin mohamed abdelmoniem <yassinmohamed022@gmail.com>
2024-11-02 03:12:48 +0200

    ocr integrated v2

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./Pages/login/login.component";
 import {RegistrationTwoComponent} from './Pages/registration/registration-two/registration-two.component';
-import {ImageUploadComponent} from '../../app/image-upload/image-upload.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegistrationTwoComponent, ImageUploadComponent],
+  imports: [RouterOutlet, LoginComponent, RegistrationTwoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
