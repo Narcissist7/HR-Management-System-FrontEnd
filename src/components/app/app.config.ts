77d66fc78c12ab425c722fba1385b995a8629880
Narcissist7 <aa.abuelezz@outlook.com>
2024-11-04 00:08:08 +0200

    Fixing conflicts

--- src/components/app/app.config.ts
+++ src/components/app/app.config.ts
@@ -2,17 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-<<<<<<< HEAD
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideHttpClient(),provideClientHydration()]
-=======
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
->>>>>>> 447ef6c2c6ebe307c84141f8b6d30088fc49030f
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
+    provideHttpClient(),provideClientHydration()]
 };
