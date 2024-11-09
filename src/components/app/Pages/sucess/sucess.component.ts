d8512b444b13de56a99acc773efb5f8e060b8a99
Habibamourad62 <habibaabdelgawad62@gmail.com>
2024-11-09 21:56:49 +0200

    success page done

--- src/components/app/Pages/sucess/sucess.component.ts
+++ src/components/app/Pages/sucess/sucess.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-sucess',
   standalone: true,
   imports: [],
   templateUrl: './sucess.component.html',
-  styleUrl: './sucess.component.css'
+  styleUrl: './sucess.component.css',
+
 })
 export class SucessComponent {
 
 }
+
+
+
+
