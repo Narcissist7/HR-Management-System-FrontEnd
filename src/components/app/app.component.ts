1ddc3d8634274cb74f2383b4e52766778f785805
yassin mohamed abdelmoniem <yassinmohamed022@gmail.com>
2024-12-05 17:16:01 +0200

    unwanted files

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./Pages/login/login.component";
 import {CandidateComponent} from './Pages/registration/candidate/candidate.component';
-import { JobListComponent } from "./Pages/Admin-Job-view/admin-job-view.component";
+
 import { HttpClientModule } from '@angular/common/http';
 import {HomeComponent} from './Pages/home/home.component';
 import {ToastModule} from 'primeng/toast';
-// import { ChartModule } from 'primeng/chart';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, CandidateComponent, JobListComponent, HttpClientModule, HomeComponent , ToastModule],
+  imports: [RouterOutlet, LoginComponent, CandidateComponent, HttpClientModule, HomeComponent , ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
-import { LayoutComponent } from './Pages/layout/layout.component';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 import { CandidateComponent } from './Pages/registration/candidate/candidate.component';
-import { JobListComponent } from './Pages/Admin-Job-view/admin-job-view.component';
-import { UserprofileComponent } from './Pages/userprofile/userprofile.component';
 import { HomeComponent } from './Pages/home/home.component';
 import {VisitorComponent} from './Pages/registration/visitor/visitor.component';
 import {LogEntryComponent} from './Pages/registration/log-entry/log-entry.component';
 import {SucessComponent} from './Pages/sucess/sucess.component';
 import {FailedComponent} from './Pages/failed/failed.component';
-import {AlreadyexistsComponent} from './Pages/alreadyexists/alreadyexists.component';
 import {CandidateListComponent} from '../candidate-list/candidate-list.component';
 import {CandidateDetailsComponent} from '../candidate-details/candidate-details.component';
 import {VisitorListComponent} from '../visitor-list/visitor-list.component';
@@ -49,22 +45,8 @@ export const routes: Routes = [
     component: LoginComponent
   },
   {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-      }
-    ]
-  },
-  {
     path: 'registration-two',
     component: CandidateComponent
-  },
-  {
-    path: 'userprofile',
-    component: UserprofileComponent
   },
   {
     path: 'visitor',
@@ -81,10 +63,6 @@ export const routes: Routes = [
   {
     path: 'failed',
     component: FailedComponent
-  },
-  {
-    path: 'alreadyexists',
-    component: AlreadyexistsComponent
   },
   {
     path: 'candidateList',
